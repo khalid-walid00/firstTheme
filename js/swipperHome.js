7849570f6e3eb05ed1ca25ef9d6e8c65fbeb4f68
khalidwalid00 <khalidk50050@gmail.com>
2024-07-20 19:25:23 +0300

    first theme majarah

---
+++ js/swipperHome.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".swipperHome", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    slideOffsetBefore: 50, 
+    slideOffsetAfter: 50, 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swipperHome-button-next",
+      prevEl: ".swipperHome-button-prev",
+      hideOnClick: false,
+    },
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        color: {
+             green:"#266526"
+          },
+      },
+    },
+    plugins: [],
+  }
